8cfb6ae8acc867930b8c75bf6409c91bcc38e987
Ajay Pradhan <azaypdn@gmail.com>
2021-06-11 20:01:29 +0530

    added icon, scroll behaviour, page titile

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
   const field1 = document.querySelector(".menubutton");
   const field2 = document.querySelector(".menubutton__line");
   const field3 = document.querySelector(".mobile_nav");
+  const n1 = document.getElementById("mob-nav-links1");
+  const n2 = document.getElementById("mob-nav-links2");
+  const n3 = document.getElementById("mob-nav-links3");
+  const n4 = document.getElementById("mob-nav-links4");
   // mobile navigation state
   let isAnimate = false;
-  field1.addEventListener("click", () => {
+  const toggle = () => {
     if (isAnimate) {
       field1.classList.remove("animate");
       field2.classList.remove("animate");
@@ -16,5 +20,10 @@
     field2.classList.add("animate");
     field3.classList.add("active");
     isAnimate = !isAnimate;
-  });
+  };
+  field1.addEventListener("click", toggle);
+  n1.addEventListener("click", toggle);
+  n2.addEventListener("click", toggle);
+  n3.addEventListener("click", toggle);
+  n4.addEventListener("click", toggle);
 })();
